7ce796f539d87e0ee18a348c42db6d2eac3a5ae8
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-07-09 18:41:37 -0400

    Adding Assignment 1.4 code attribution

--- week-1/ts-examples/person.class.ts
+++ week-1/ts-examples/person.class.ts
@@ -1,3 +1,13 @@
+// /*
+// ============================================
+// ; Title: Assignment 1.4
+// ; Author: Professor Krasso
+// ; Date: 09 July 2020
+// ; Modified By: Jonathan Kobyluck
+// ; Description: TypeScript
+// ;===========================================
+// */
+
 import { IPerson } from "./person.interface";
 
 class Person implements IPerson {
